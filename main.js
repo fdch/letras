cc3ace544aaec083be9b20a994477c13b03f8176
Fede Camara Halac <camarafede@gmail.com>
2018-02-09 00:01:26 +0100

    updates

---
+++ main.js
@@ -0,0 +1,62 @@
+var url = "https://fdch.github.io/letras";
+var repo = "https://github.com/fdch/letras";
+var email = "fch226@nyu.edu";
+var title = "Letras";
+var subtitle = "...";
+var https = "https://spreadsheets.google.com/feeds/list/";
+var altjson = "/public/values?alt=json";
+var w, h;
+var spreadsheetID = "1umuxkyz_4xrxgX_qS-tmOMK7CqGuNS67KFzRhFGuP5A/";
+var titleData = "\
+<header>\
+<h1 onclick=\"location.href='"+ url + "'\">"+ title +"</h1>\
+<h2 onclick=\"location.href='" + url + "'\">" + subtitle + "</h2>\
+</header>";
+var containers = "<div id=content></div>";
+var lit = https + spreadsheetID + "1" + altjson;
+
+function linkify(x)
+{
+  return(x.replace(/ /g,"_").toLowerCase());
+}
+
+function makeHref(x)
+{
+  return("<a href=\"#"+linkify(x)+"\">"+x+" </a>");
+}
+
+function loadJSON(x,callback)
+{
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', x, true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);  
+}
+
+words = [];
+
+function getLit(sheet, arr)
+{
+  loadJSON(sheet, function(response) {
+    var f = JSON.parse(response);
+    var entry = f.feed.entry;
+    for (var i in entry)
+    {
+      var e = entry[i];
+      arr.push(e.gsx$a.$t);
+    }
+  });
+}
+
+
+$(document).ready(function(x) {
+  if ((w = $(window).width()) >= 600) w = w*0.5;
+  h = $(window).height();
+  $("body").append([titleData, containers]);
+  getLit(lit, words);
+});
