6d44ac9bc54776974ed9384d48d41d2e316fd37f
Fede Camara Halac <camarafede@gmail.com>
2018-02-09 00:06:47 +0100

    updates

--- main.js
+++ main.js
@@ -59,5 +59,5 @@ $(document).ready(function(x) {
   h = $(window).height();
   $("body").append([titleData, containers]);
   getLit(lit, words);
-  $("#content").append[words];
+  $("#content").append["<p>"+words[i]+"</p>"];
 });
